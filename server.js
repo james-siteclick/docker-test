b720d4d6137eb6d79347c0f23af5de9b94de8624
James Carmichael <james@siteclick.co.uk>
2021-03-13 09:28:06 +0000

    Docker hapi app

---
+++ server.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// Constants
+const PORT = 8080;
+const HOST = "0.0.0.0";
+
+const Hapi = require("@hapi/hapi");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: PORT,
+    host: HOST,
+  });
+
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return "Hello World (hapi)!";
+    },
+  });
+
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+};
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
